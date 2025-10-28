987f4821cd3f7e2011f0019946cb3e5b00f70ddd
Ana Lemos <carol1692@hotmail.com>
2025-10-28 19:36:14 -0300

    :tada: Commit configurações iniciais

---
+++ src/main/java/com/maventech/elocrm/ElocrmApplication.java
@@ -0,0 +1,13 @@
+package com.maventech.elocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElocrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElocrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maventech/elocrm/ElocrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maventech.elocrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElocrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
