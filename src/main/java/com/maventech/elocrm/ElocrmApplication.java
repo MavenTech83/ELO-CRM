f3a3d48cec720ea55d4a7750e3cdf34c83ad1ddb
Ana Lemos <carol1692@hotmail.com>
2025-10-28 20:29:58 -0300

    Adição configuração de variáveis de ambiente

--- src/main/java/com/maventech/elocrm/ElocrmApplication.java
+++ src/main/java/com/maventech/elocrm/ElocrmApplication.java
@@ -3,11 +3,23 @@ package com.maventech.elocrm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ElocrmApplication {
 
 	public static void main(String[] args) {
+
+		try {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		} catch (Exception e) {
+			System.out.println("Executando sem arquivo .env - usando variáveis de ambiente do sistema.");
+		}
+	
 		SpringApplication.run(ElocrmApplication.class, args);
+	
 	}
 
 }
