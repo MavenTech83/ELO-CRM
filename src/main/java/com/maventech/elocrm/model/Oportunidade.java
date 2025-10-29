9b825da87b7bd259e130756c26f65398538eb69d
Thalitalima23 <thalitalima23@gmail.com>
2025-10-29 10:35:03 -0300

    :sparkles: Ajustes

--- src/main/java/com/maventech/elocrm/model/Oportunidade.java
+++ src/main/java/com/maventech/elocrm/model/Oportunidade.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -40,13 +42,13 @@ public class Oportunidade {
     
     //Relacionamentos//
     
-   //ManyToOne
-   //JoinColumn(name = "usuario_id")
-  //private Usuario usuario;
+   @ManyToOne
+   @JoinColumn(name = "usuario_id")
+  private Usuario usuario;
 
-   //ManyToOne
-  //@JoinColumn(name = "cliente_id")
-  //private Cliente cliente;
+   @ManyToOne
+  @JoinColumn(name = "cliente_id")
+  private Cliente cliente;
     
 
     
